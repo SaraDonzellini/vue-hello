eba33ab7acbb25a9ad0a8dc140ac03e9636efb8f
SaraDonzellini <saradonzie@gmail.com>
2022-12-12 15:21:07 +0100

    exercise complete

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 
   const { createApp } = Vue
+  
 
   createApp({
     data() {
       return {
         title: 'Hello Vue!',
-        image: '../img/vuehello.png'
+      
       }
     },
     
   }).mount('#app')
-  
